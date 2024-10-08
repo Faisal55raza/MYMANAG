f4ec73c7bb0dd718316ba7103035aceafbf69ba2
Faisal <faishal.55raza@gmail.com>
2024-10-09 03:10:56 +0530

    two

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:4000"
-export const IF = "http://localhost:4000/images/"
+export const URL = "https://faisal-blog-backend.vercel.app"
+export const IF = "https://faisal-blog-backend.vercel.app/images/"
