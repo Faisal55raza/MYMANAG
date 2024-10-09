49ec2157647b6716d8e7e2c487f1bf4a72db301c
Faisal <faishal.55raza@gmail.com>
2024-10-09 16:22:54 +0530

    testing

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const connectDB = async()=>{
 dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:["https://miscellineous.vercel.app","http://localhost:5173"], credentials:true}))
+app.use(cors({origin:["http://localhost:5173"], credentials:true}))
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
 app.use("/api/posts",postRoute)
