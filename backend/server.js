37404ea60e7a7b8c1909c6851470e4d670974360
Faisal <faishal.55raza@gmail.com>
2024-10-09 16:56:58 +0530

    two

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const connectDB = async()=>{
 dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:["http://localhost:5173"],    
+app.use(cors({origin:"http://localhost:5173",    
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "credentials","withCredentials"],
     credentials: true
