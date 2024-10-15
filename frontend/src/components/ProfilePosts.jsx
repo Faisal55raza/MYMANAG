06fd03e6ee37c4de1075918e48a2a4e80209a877
Faisal <faishal.55raza@gmail.com>
2024-10-15 08:58:48 +0530

    profile post updated

--- frontend/src/components/ProfilePosts.jsx
+++ frontend/src/components/ProfilePosts.jsx
@@ -6,7 +6,7 @@ const ProfilePosts = ({p}) => {
         <div className="w-full flex mt-8 space-x-4">
             {/* left */}
             <div className="w-[35%] height-[200px] flex justify-center items-center">
-                <img src={IF+p.photo} alt="" className="h-full w-full object-cover" />
+                <img src={p.photo} alt="" className="h-full w-full object-cover" />
             </div>
 
             {/* right */}
